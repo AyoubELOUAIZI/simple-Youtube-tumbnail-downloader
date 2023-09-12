8cd7e221221fe60e64bd7611dea23922f2826a2f
AyoubELOUAIZI <ayoubelouaizi@gmail.com>
2023-09-12 09:55:45 +0100

    google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,6 +13,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+       <head>
+       {/* <!-- Google tag (gtag.js) --> */}
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-QKJ264BEE0"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QKJ264BEE0');
+</script>    
+  </head>
       <body className={inter.className}>
         <Navbar/>
         {children}
